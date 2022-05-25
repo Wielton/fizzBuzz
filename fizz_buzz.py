e6602e64722f82f41b5ed6f15af5ca5a4928286f
Wielton <grantwielgosz@gmail.com>
2022-05-24 23:58:37 -0600

    added the list of numbers, function works well

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,11 +1,13 @@
-def fizzy(num):
-    if num % 3 == 0 and num % 5 == 0:
-        print("FizzBuzz")
-    elif num % 5 == 0:
-        print("Buzz")
-    elif num % 3 == 0:
-        print("Fizz")
-    else:
-        print("This number isn't divisible by 3 or 5")
+def fizzy():
+    for num in nums:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print("This number isn't divisible by 3 or 5")
 
-print(fizzy(30))
+nums = [2,3,4,5,8,10,13,15,25,30,45,48,50,52,56,64,72]
+print(fizzy())
